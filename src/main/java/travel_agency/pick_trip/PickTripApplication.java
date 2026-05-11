e0d8d96a9bd6c0e5493d0438dc25b5345d28368e
HyeonJun <hyeonjun1968@naver.com>
2026-05-11 22:19:39 +0900

    chore: project init

---
+++ src/main/java/travel_agency/pick_trip/PickTripApplication.java
@@ -0,0 +1,13 @@
+package travel_agency.pick_trip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickTripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickTripApplication.class, args);
+    }
+
+}
---
+++ src/test/java/travel_agency/pick_trip/PickTripApplicationTests.java
@@ -0,0 +1,13 @@
+package travel_agency.pick_trip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickTripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
